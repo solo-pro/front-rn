ab45ac85e434fd2c71b500e8f175997a7138147d
tjdanqkr <tjdanqkr@gmail.com>
2024-07-30 17:15:56 +0900

    [feat] product mock data make

---
+++ type/product.ts
@@ -0,0 +1,25 @@
+type Product = {
+  id: number;
+  name: string;
+  category: string;
+  price: number;
+  rating: number;
+  reviews: number;
+  stock: number;
+  description: string;
+  image: string;
+  createdAt: string;
+};
+type Category = {
+  id: number;
+  name: string;
+  image: string;
+};
+type ProductReview = {
+  id: number;
+  productId: number;
+  rating: number;
+  text: string;
+  createdAt: string;
+};
+export { Category, Product, ProductReview };
